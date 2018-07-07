f6513653c18e096569d27b29af05781508974412
shunsukesuzuki <shunsukesuzuki@192.168.0.17>
2018-07-07 17:13:50 +0900

    first commit

---
+++ nest/src/main/java/com/nest/NestApplication.java
@@ -0,0 +1,12 @@
+package com.nest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NestApplication.class, args);
+  }
+}
