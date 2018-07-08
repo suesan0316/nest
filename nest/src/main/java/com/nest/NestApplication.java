6ff16111ff500a353a4bd6d3552508d6998427c9
shunsukesuzuki <shunsukesuzuki@192.168.0.17>
2018-07-08 11:13:47 +0900

    login control add

--- nest/src/main/java/com/nest/NestApplication.java
+++ nest/src/main/java/com/nest/NestApplication.java
@@ -2,9 +2,10 @@ package com.nest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NestApplication {
+public class NestApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(NestApplication.class, args);
---
+++ nest/src/main/java/com/nest/control/LoginController.java
@@ -0,0 +1,13 @@
+package com.nest.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+  @GetMapping("/login")
+  public String login(Model model) {
+    return "login";
+  }
+}
